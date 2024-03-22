a0bbe80915f7813795f831aa637ad7129951480d
João Malvezzi <132956310+Malvitos@users.noreply.github.com>
2024-03-22 15:53:32 -0300

    config_forca

--- forca.py
+++ forca.py
@@ -1,11 +1,16 @@
 import random
 
-#cria uma lista de palavras que serão sorteadas
-
-palavras= ['python','programacao','computador','internet','disciplina','motherboard']
+#cria uma lista de palavras que serao sorteadas
+palavras = ['python','programacao','computador','internet','disciplina','motherboard','mouse']
 
 #escolher uma das palavras
-
 palavra_sorteada = random.choice(palavras)
-print(palavra_sorteada)
+
+#string com as lacunas das letras
+palavra_escondida = '-' * len(palavra_sorteada)
+
+#lista vazia para armazenar as letras que ja foram faladas
+letras_adivinhadas = []
+
+max_tentativas = 6
 
