fbf3918ad938fe3070811810026fc62e3641de89
João Malvezzi <132956310+Malvitos@users.noreply.github.com>
2024-03-22 09:19:03 -0300

    First Change

---
+++ forca.py
@@ -0,0 +1,11 @@
+import random
+
+#cria uma lista de palavras que serão sorteadas
+
+palavras= ['python','programacao','computador','internet','disciplina','motherboard']
+
+#escolher uma das palavras
+
+palavra_sorteada = random.choice(palavras)
+print(palavra_sorteada)
+
