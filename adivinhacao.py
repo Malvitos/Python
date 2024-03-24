e2c3b6af3daa6ae8d5095dd5dd6b604d40a44c43
João Malvezzi <132956310+Malvitos@users.noreply.github.com>
2024-03-24 19:39:41 -0300

    adivinhacao

---
+++ adivinhacao.py
@@ -0,0 +1,21 @@
+import random
+
+numero_minimo = int(input('Qual o número mínimo do intervalo?\n'))
+numero_maximo = int(input('Qual o número máximo do intervalo?\n'))
+
+tentativas = int(input('Quantas tentativas para acertar?\n'))
+
+numero_sorteado = random.randint(numero_minimo, numero_maximo)
+
+while True:
+    tentativa = int(input(f'Digite um número entre {numero_minimo} e {numero_maximo}.\n'))
+
+    if tentativa == numero_sorteado:
+        print('Parabéns Você Acertou!')
+        break
+else: tentativas -= 1
+if tentativas == 0:
+    print(f'Você Perdeu! O número correto era {numero_sorteado}')
+
+else: 
+    print(f'Numero Errado! Tente Novamente. Você ainda tem {tentativas} tentativas.')
